c11f0c87e948ed3a3cd9be9c2e42e26a07754eb7
Dmitry Lambrianov <e-dmitry.lambrianov@smart.com>
2024-11-10 23:48:08 +0100

    ai:courses: build gpt from scratch

--- setup-hooks.sh
+++ setup-hooks.sh
@@ -3,5 +3,7 @@
 
 echo "Copying commit-msg hook to .git/hooks directory..."
 cp .githooks/commit-msg .git/hooks/commit-msg
+cp .githooks/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/commit-msg
+chmod +x .git/hooks/pre-commit
 echo "commit-msg hook has been installed."
---
+++ src/md/ai:video:building-gpt-from-scratch.md
@@ -0,0 +1,3 @@
+# Let's build GPT: from scratch, in code, spelled out (Andrej Karpathy)
+
+[Building GPT From Scratch](https://www.youtube.com/watch?v=kCc8FmEb1nY)
